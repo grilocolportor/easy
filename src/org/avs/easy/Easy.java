a3eac4ee54368f59a5707a39d144cf2ba4a93c77
VENANCIO <VENANCIO@192.168.1.9>
2013-06-16 12:32:17 -0300

    Mapa funcionando

--- src/org/avs/easy/Easy.java
+++ src/org/avs/easy/Easy.java
@@ -1,15 +1,27 @@
 package org.avs.easy;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 
-public class Easy extends Activity {
+public class Easy extends FragmentActivity  {
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_easy);
+		
+		FragmentManager fmanager = getSupportFragmentManager();
+        Fragment fragment = fmanager.findFragmentById(R.id.map);
+        SupportMapFragment supportmapfragment = (SupportMapFragment)fragment;
+        GoogleMap supportMap = supportmapfragment.getMap();
+	 
+		
 	}
 
 	@Override
